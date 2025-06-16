ab1bcdf2e5d7f634e9d7c6b9c9b0f20295608682
Songeun Kim <sok610@berkeley.edu>
2025-06-15 20:55:42 -0700

    Changed ALLOWED_HOSTS. There was a typo.

--- soundlog/settings.py
+++ soundlog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
 
 
 # Application definition
